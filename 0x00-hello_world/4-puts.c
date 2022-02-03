43844ecc9ca40ee0705c30e35536fe9a30a59210
hichem <hichemmaatoug55@gmail.com>
2022-02-03 03:45:17 -0800

    helloputs

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char world[50] = "Programming is like building a multilingual puzzle";
+puts(world);
+return (0);
+}
