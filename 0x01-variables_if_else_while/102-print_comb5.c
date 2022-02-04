a259bff680695be52d14ddafa99192c78f86d11d
hichem <hichemmaatoug55@gmail.com>
2022-02-04 12:17:04 -0800

    4 numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a, b, c, d;
+a = '0';
+while (a <= '9')
+{
+b = '0';
+while (b <= '9')
+{
+c = a;
+while (c <= '9')
+{
+d = b + 1;
+while (d <= '9')
+{
+if (a + b != c + d)
+{
+putchar(a);
+putchar(b);
+putchar(' ');
+putchar(c);
+putchar(d);
+if (a + b + c + d != 227)
+{
+putchar(',');
+putchar(' ');
+}
+}
+d++;
+}
+c++;
+}
+b++;
+}
+a++;
+}
+putchar ('\n');
+return (0);
+}
