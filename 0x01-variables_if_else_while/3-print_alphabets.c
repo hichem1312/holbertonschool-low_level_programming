7b20d3960e58a73725c8ac3d2554c80bcd2cdc46
hichem <hichemmaatoug55@gmail.com>
2022-02-04 04:15:04 -0800

    mol

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch, n, chm;
+ch = 'a';
+while (ch <= 'z')
+{
+putchar (ch);
+ch++;
+}
+chm = 'Z';
+while (chm <= 'Z')
+{
+putchar (chm);
+chm++;
+}
+n = '\n';
+putchar (n);
+return (0);
+}
