4f3a70d4b6de294c84af3d49a09b3a1784031d88
hichem <hichemmaatoug55@gmail.com>
2022-02-04 10:45:30 -0800

    combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int a, b, x;
+a = '0';
+x = '0';
+while (a != '9')
+{
+b = x;
+while (b <= '9')
+{
+if (a != b )
+{
+putchar(a);
+putchar(b);
+putchar(',');
+putchar(' ');
+}
+b++;
+}
+x++;
+a++;
+}
+putchar ('\n');
+return (0);
+}
