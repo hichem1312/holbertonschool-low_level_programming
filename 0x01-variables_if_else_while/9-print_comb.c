a92f71af97199ac13d0686a3cce2cd1bde21047c
hichem <hichemmaatoug55@gmail.com>
2022-02-04 05:52:27 -0800

    easy

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+i = '0';
+while (i <= '9')
+{
+putchar(i);
+if (i != '9')
+{
+putchar(',');
+putchar(' ');
+}
+i++;
+}
+putchar ('\n');
+return (0);
+}
