ae90e5a40d3777a3f62edfbadbdade4110d284b2
hichem <hichemmaatoug55@gmail.com>
2022-02-08 05:42:41 -0800

    az

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - test the sign of number
+ * @n: ll
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
