ca08187161fd5ba991e55c28117361d845e1beef
hichem <hichemmaatoug55@gmail.com>
2022-02-08 09:21:31 -0800

    eeee

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * jack_bauer - test the sign of number
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void jack_bauer(void)
+{
+char i, j, k, l;
+for (i = '0'; i <= '2'; i++)
+{
+for (j = '0'; j <= '3'; j++)
+{
+for (k = '0'; k <= '5'; k++)
+{
+for (l = '0'; l <= '9'; l++)
+{
+_putchar(i);
+_putchar(j);
+_putchar(':');
+_putchar(k);
+_putchar(l);
+_putchar('\n');
+}
+}
+}
+}
+}
