15dd32ef7d3fce0470038969d47005d27a0d0f90
hichem <hichemmaatoug55@gmail.com>
2022-02-08 14:31:54 -0800

    r

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,48 +7,30 @@
  */
 void times_table(void)
 {
-int i, p, s, d, k, v;
-char x;
-for (k = 0; k <= 9; k++)
+int j, i, a, b, v;  
+for (i = 0; i <= 9; i++)
 {
-_putchar('0');
-if (k != 9)
-{
-_putchar(',');
-_putchar(' ');
+    for (j = 0; j <= 9; j++)
+    {
+	v = j * i;
+	if (v >= 10)
+	{
+		a = v % 10;
+		b = v / 10;
+		_putchar(b + 48);
+		_putchar(a + 48);
+	}
+	else
+	{
+	_putchar(' ');
+        _putchar(v + 48);
+	}
+	if (j != 9)
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
+	}
+	_putchar('\n');
 }
 }
-_putchar('\n');
-v = 0;
-for (i = 0; i < 9; i++)
-{
-p = v + 1;
-for (x = 0; x <= 9; x++)
-{
-if (x == 0)
-{
-_putchar('0');
-}
-else
-{
-p *= x;
-if (p >= 9)
-{
-d = p % 10;
-s = p / 10;
-_putchar(s);
-_putchar(d);
-}
-else
-{
-_putchar(p);
-}
-}
-_putchar(',');
-_putchar(' ');
-}
-_putchar('\n');
-v++;
-}
-}
-
