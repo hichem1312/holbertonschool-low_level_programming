491108b5e196d7c165304386f3742cfd4700c73c
hichem <hichemmaatoug55@gmail.com>
2022-02-08 13:28:37 -0800

    w

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,26 +1,30 @@
 #include "main.h"
 /**
  * times_table - writes the character c to stdout
- * 
+ *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 void times_table(void)
 {
-int i;
-char x;
+int i, p, s, d, k;
+char x, v;
 for (i = 0; i <= 10; i++)
 {
+v = '0';
 if (i == 0)
+{
+for (k = 0; k <= 9; k++)
 {
 _putchar('0');
 _putchar(',');
 _putchar(' ');
+}
 _putchar('\n');
 }
 else
 {
-p = '0';
+p = v + 1;
 for (x = '0'; x <= '9'; x++)
 {
 p *= x;
@@ -28,18 +32,19 @@ if (p >= '9')
 {
 d = p % 10;
 s = p / 10;
-_putchar(s);
-_putchar(d);
+_putchar(s + 48);
+_putchar(d + 48);
 }
 else
 {
-_putchar(b);
+_putchar(p + 48);
 }
 _putchar(',');
 _putchar(' ');
 p++;
 }
+_putchar('\n');
+}
+v++;
 }
 }
-}
-
