1e7488b856bb80ada2db7b63f4fbdb8f42bedd45
hichem <hichemmaatoug55@gmail.com>
2022-02-08 08:34:18 -0800

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - writes the character c to stdout
+ * @i:last
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int print_last_digit(int i)
+{
+int e;
+e = i % 10;
+return (e);
+}
