d17ca6110379834c139a93312660398185de6e4a
hichem <hichemmaatoug55@gmail.com>
2022-02-08 11:15:47 -0800

    q

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,37 +8,21 @@
  */
 void jack_bauer(void)
 {
-char i, j, k, l, y, x, o;
+char i, j, k, l;
 for (i = '0'; i <= '2'; i++)
 {
-j = 0;
-while (j <= '9' && i < 2)
+for (j = '0'; j <= '9'; j++)
 {
 for (k = '0'; k <= '5'; k++)
 {
 for (l = '0'; l <= '9'; l++)
 {
+if (
 _putchar(i);
 _putchar(j);
 _putchar(':');
 _putchar(k);
 _putchar(l);
-_putchar('\n');
-}
-}
-j++;
-}
-for (y = '0'; y <= '3'; y++)
-{
-for (x = '0'; x <= '5'; x++)
-{
-for (o = '0'; o <= '9'; o++)
-{
-_putchar(i);
-_putchar(y);
-_putchar(':');
-_putchar(x);
-_putchar(o);
 _putchar('\n');
 }
 }
