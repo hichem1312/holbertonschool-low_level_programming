479fab01b1c1702581a6e7f8a6c764aeb62a44b7
hichem <hichemmaatoug55@gmail.com>
2022-02-08 05:13:13 -0800

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,19 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * int _islower - test the character lowercase 
- *
+ * _islower - test the character lowercase
+ * @c: ll
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _islower(int c)
-	{
-		if (c >= 97 && c <= 122)
-			{
-				return(1);
-			}
-		else
-			{
-				return(0);
-			}
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+else
+{
+return (0);
+}
 }
