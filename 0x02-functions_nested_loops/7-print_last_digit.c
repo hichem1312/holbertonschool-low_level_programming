37aae264fad251bf694877f4cae0fc2f9c75bae3
hichem <hichemmaatoug55@gmail.com>
2022-02-08 10:40:47 -0800

    r

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,7 @@ int e, v, b;
 if (i == 0)
 {
 _putchar('0');
-return (48);
+return (0);
 }
 else if (i <= 0)
 {
@@ -19,13 +19,13 @@ e = i * (-1);
 v = e % 10;
 b = v + 48;
 _putchar(b);
-return (b);
+return (v);
 }
 else
 {
 v = i % 10;
 b = v + 48;
 _putchar(b);
-return (b);
+return (v);
 }
 }
