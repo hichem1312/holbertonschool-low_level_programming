7f2f70c381d6fd4044414e3c48954f97e894c0fe
hichem <hichemmaatoug55@gmail.com>
2022-02-08 04:17:01 -0800

    alpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_alphabet_x10 - writes the character c to stdout
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_alphabet_x10(void)
+{
+	int x, i;
+	for(i=0;i<10;i++)
+	{	
+	x = 'a';
+	while	(x <= 'z')
+		{
+		_putchar(x);
+		x++;
+		}
+		_putchar('\n');
+	}
+}
