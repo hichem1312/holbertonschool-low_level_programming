3352c15a6c7af4c21a3b133233e687d342a8bc38
hichem <hichemmaatoug55@gmail.com>
2022-02-09 12:49:34 -0800

    s

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,17 +5,13 @@
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int addition(int s)
+int addition(void)
 {
 int x;
 s = 0;
 for (x = 0 ; x <= 1024 ; x++)
 {
-if (x % 3 == 0)
-{
-s += x;
-}
-if (x % 5 == 0)
+if ((x % 3 == 0) || ( x % 5 == 0))
 {
 s += x;
 }
