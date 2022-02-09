b077610001512c20289d53b23c3e890a2aefd15f
hichem <hichemmaatoug55@gmail.com>
2022-02-09 04:34:42 -0800

    add

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
  * addition - writes the character c to stdout
- * 
+ * @s:integer
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-void addition(void)
+int addition(int s)
 {
-int x, s;
+int x;
 s = 0;
 for (x = 0 ; x <= 1024 ; x++)
 {
