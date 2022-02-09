e782a72b193cafb6755a8154d93b6004983b0ec3
hichem <hichemmaatoug55@gmail.com>
2022-02-09 09:10:25 -0800

    head

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,9 +1,8 @@
-carrie@ubuntu:/debugging$ cat main.h
 #ifndef MAIN_H
 #define MAIN_H
 
 #include <stdio.h>
-
 void positive_or_negative(int i);
 int main(void);
+int largest_number(int a, int b, int c);
 #endif /* MAIN_H */
