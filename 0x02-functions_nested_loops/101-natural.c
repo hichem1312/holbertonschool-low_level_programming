8ff4b2e6f2694a9cbba013612fce400c150b117a
hichem <hichemmaatoug55@gmail.com>
2022-02-09 04:23:58 -0800

    add

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * addition - writes the character c to stdout
+ * @a:integer
+ * @b:integer
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int addition(int a, b)
+{
+int x, s;
+s = 0
+for (x = 0 ; x <= 1024 ; x++)
+{
+if (x % 3 == 0)
+{
+s += x;
+}
+if (x % 5 == 0)
+{
+s += x;
+}
+}
+return (x);
+}
