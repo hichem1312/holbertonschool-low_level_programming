9edeb7cfc7ba008d9d61beb85b628a16fdee9bd5
hichem <hichemmaatoug55@gmail.com>
2022-02-09 09:35:31 -0800

    s

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,12 +8,17 @@
 int main(void)
 {
 int i;
+
 printf("Infinite loop incoming :(\n");
+
 i = 0;
+
 while (i < 10) /*this command */
 { /*this command */
 putchar(i); /*this command */
 }  /*this command */
+
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
