43da26aa6725a2b5ccc8f62791c1a1f1ec2379aa
hichem <hichemmaatoug55@gmail.com>
2022-02-09 00:31:11 -0800

    m

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_to_98 - writes the character c to stdout
+ *@n:integer
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_to_98(int n)
+{
+while (n != 98)
+{
+if (n > 98)
+{
+printf ("%d, ",n);
+n--;
+}
+else if (n < 98)
+{
+printf ("%d, ",n);
+n++;
+}
+}
+printf ("98");
+printf ("\n");
+}
