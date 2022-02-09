bb6af94050b142151f7e476007d6b98cbc09e3c1
hichem <hichemmaatoug55@gmail.com>
2022-02-09 14:04:03 -0800

    98

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+long int s, nb, nb1;
+nb = 0;
+nb1 = 1;
+for (i = 0; i <= 98; i++)
+{
+if (i >= 1)
+{
+s = nb + nb1;
+nb = nb1;
+nb1 = s;
+printf("%ld", s);
+if (i != 50)
+{
+printf(", ");
+}
+}
+}
+printf("\n");
+return (0);
+}
