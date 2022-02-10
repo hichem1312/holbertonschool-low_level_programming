78078fb3bd2f5d66223c02e730e46140c740aa31
hichem <hichemmaatoug55@gmail.com>
2022-02-10 05:38:43 -0800

    q

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - writes the character c to stdout
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+_putchar(i + 48);
+}
+_putchar('\n');
+}
