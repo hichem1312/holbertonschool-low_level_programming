e0fc8a97947f83a2daafcf1e8dbe9f746e2ee2ac
hichem <hichemmaatoug55@gmail.com>
2022-02-10 05:53:17 -0800

    q

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - writes the character c to stdout
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_most_numbers(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+if (i != 2 && i != 4)
+{
+_putchar(i + 48);
+}
+}
+_putchar('\n');
+}
