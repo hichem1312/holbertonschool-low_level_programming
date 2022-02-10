6757360505dd239b17d2f3808d1f836c1a750d0b
hichem <hichemmaatoug55@gmail.com>
2022-02-10 05:55:17 -0800

    q

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_numbers - writes the character c to stdout
+ * print_most_numbers - writes the character c to stdout
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
