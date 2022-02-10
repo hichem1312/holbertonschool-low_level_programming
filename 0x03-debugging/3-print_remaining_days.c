3ed58918d6cccecb58cec4a9256e6bdd07083bcf
hichem <hichemmaatoug55@gmail.com>
2022-02-10 14:12:10 -0800

    h

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,15 +24,14 @@ printf("Remaining days: %d\n", 366 - day);
 }
 else
 {
-day -= 1;
 if ((month == 2 && day == 60) && (year % 4 == 0))
 {
-printf("Day of the year: %d\n", day + 1);
+printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
 else
 {
-printf("Day of the year: %d\n", day + 1);
+printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
 }
