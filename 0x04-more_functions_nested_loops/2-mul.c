6b3640ccff26441994bb2f08120358803aed87b4
hichem <hichemmaatoug55@gmail.com>
2022-02-10 04:00:16 -0800

    q

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - writes the character c to stdout
+ * @a:integer
+ * @b:integer
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int mul(int a, int b)
+{
+int r;
+r = a * b;
+return (r);
+}
