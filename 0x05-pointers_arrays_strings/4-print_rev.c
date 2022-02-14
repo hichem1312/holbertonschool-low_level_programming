c11a019c2504565973e670f6ea10deb248904ee5
hichem <hichemmaatoug55@gmail.com>
2022-02-14 03:44:18 -0800

    g

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_rev - writes the character c to stdout
+ * @s:integer
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_rev(char *s)
+{
+int l = 0, i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+l++;
+}
+for (j = l; j >= 0; j--)
+{
+_putchar(s[j]);
+}
+_putchar('\n');
+}
