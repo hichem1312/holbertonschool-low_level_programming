af766c46c30c3267cfe65d1620238801e70071ab
hichem <hichemmaatoug55@gmail.com>
2022-02-14 02:05:21 -0800

    long

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - writes the character c to stdout
+ * @lengt:d
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _strlen(char *s)
+{
+int lengt;
+lengt = strlen(s);
+return (lengt);
+}
