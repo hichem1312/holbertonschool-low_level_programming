70fa69c8f8aa9edb5193a9a50d80798de8a4c4b2
hichem <hichemmaatoug55@gmail.com>
2022-02-14 09:19:33 -0800

    h

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half - writes the character c to stdout
+ * @str:string
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void puts_half(char *str)
+{
+int i, l, li;
+l = strlen(str);
+if (l % 2 == 0)
+{
+li = l / 2;
+}
+else
+{
+li = (l + 3) / 2;
+}
+for (i = li; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
+}
