4c9b7316357016e9d4cfaaf06ae73c4dad83ba0d
hichem <hichemmaatoug55@gmail.com>
2022-02-14 13:24:59 -0800

    w

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,23 +10,23 @@
 int _atoi(char *s)
 {
 int str = 0, i, c = 1;
-if (strcmp(s,"-2147483648") == 0)
+if (strcmp(s, "-2147483648") == 0)
 return (-2147483648);
 for (i = 0; s[i] != '\0'; i++)
 {
 if ((s[i] >= '0' && s[i] <= '9') && (s[i + 1] < '0' || s[i + 1] > '9'))
 {
-str = str *10 - '0' + s[i];
+str = str * 10 - '0' + s[i];
 return (str *c);
 }
 else if (s[i] >= '0' && s[i] <= '9')
 {
-str = str *10 - '0' + s[i];
+str = str * 10 - '0' + s[i];
 }
 else if (s[i] == '-')
 {
 c *= -1;
 }
 }
-return (str * c);
+return (str *c);
 }
