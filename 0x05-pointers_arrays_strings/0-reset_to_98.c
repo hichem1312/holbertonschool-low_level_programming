fae3d28543ef705bd27bd216b93b2d8f68b558d1
hichem <hichemmaatoug55@gmail.com>
2022-02-14 01:45:34 -0800

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * reset_to_98 - writes the character c to stdout
+ * @n:ff
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
