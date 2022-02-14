92826a415b0b66bc467c3053fb08946bcb0b5bd9
hichem <hichemmaatoug55@gmail.com>
2022-02-14 02:19:57 -0800

    d

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * _puts - writes the character c to stdout
+ * @str:string
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void _puts(char *str)
+{
+puts(str);
+}
