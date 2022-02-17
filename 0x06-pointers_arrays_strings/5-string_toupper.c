29b4c2fa64859ad854ce38c84d001742332b6968
hichem <hichemmaatoug55@gmail.com>
2022-02-17 00:08:35 -0800

    f

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * string_toupper - writes the character c to stdou
+ * @t: 
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *string_toupper(char *t)
+{
+int l = strlen(t), i;
+for (i = 0; i < l; i++)
+{
+if (t[i] >= 97 && t[i] <= 122)
+{
+t[i] = t[i] - 32;
+}
+}
+return (t);
+}
