b78cbe0360412161f0ece764be5d0e83761c54f4
hichem <hichemmaatoug55@gmail.com>
2022-02-17 01:44:29 -0800

    cap

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+/**
+ * cap_string - writes the character c to stdout
+ * @s:string
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *cap_string(char *s)
+{
+int l = strlen(s), i;
+for (i = 0; i < l; i++)
+{
+if ((s[i] >= 97 && s[i] <= 122) && i == 0)
+{
+s[i] = s[i] - 32;
+}
+else if ((s[i] >= 97 && s[i] <= 122) && (s[i - 1] == 32 || s[i] == '	' || s[i - 1] == ',' || s[i - 1] == ';' || s[i - 1] == '.' || s[i - 1] == '!' || s[i - 1] == '?' || s[i - 1] == '\"' || s[i - 1] == '(' || s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}'))
+{
+s[i] = s[i] - 32;
+}
+}
+return (s);
+}
