0703bf2f045e4b63c903e4920ccaf5a0f86159db
hichem <hichemmaatoug55@gmail.com>
2022-02-21 14:45:44 -0800

    die

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <string.h>
+/**
+ * print_diagsums - copy tow strings
+ * @a:string
+ * @size:string
+ * @n:integer
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void print_diagsums(int *a, int size)
+{
+	int i, s1, s2, j;
+i = 0;
+j = size -1;
+s1 = 0;
+s2 = 0;
+	while (i < (size * size))
+	{
+		s1 = s1 + a[i];
+		i = i + size + 1;
+	}
+	while (j < (size * size - 1))
+	{
+		s2 = s2 + a[j];
+		j = j + size - 1;
+	}
+	printf("%d, ", s1);
+	printf("%d\n", s2);
+}
