7fa20a4cdbf5c38413245d527ad2ed196aeedbb1
hichem <hichemmaatoug55@gmail.com>
2022-02-21 11:46:11 -0800

    d

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strspn - d
+ * @s:string
+ * @accept:string
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+int l = strlen(s), l1 = strlen(accept), i, j;
+unsigned int u;
+u = 0;
+for (i = 0; i < l; i++)
+{
+j = 0;
+while (s[i] != accept[j] && j < l1)
+{
+j++;
+}
+if (s[i] == accept[j])
+{
+u += 1;
+}
+else
+{
+i = l;
+j = l1;
+}
+}
+return (u); 
+}
