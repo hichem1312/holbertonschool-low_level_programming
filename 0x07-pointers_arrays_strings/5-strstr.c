4b837575257523fead47efad94bacb267b81d13c
hichem <hichemmaatoug55@gmail.com>
2022-02-22 11:04:10 -0800

    g

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,31 +8,40 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 char *_strstr(char *haystack, char *needle)
-{
-char g[10000];
-int j, i, v = 0, l = strlen(needle), l1 = strlen(haystack), k, m;
+{char g[10000];
+int s, p, j, i, y, v = 0, l = strlen(needle), 
+    l1 = strlen(haystack), k, m;
 for (i = 0; i < l; i++)
 {
 for (j = 0; j < l1; j++)
 {
 if (haystack[j] == needle[i])
 {
+p = 0;
+s = 0;
+for (k = j; k < l + j && s != l; k++)
+{
+if (haystack[k] == needle[p])
+{
+s = s + 1;
+p++;
+}
+if (s == l)
+{
 v = j;
 i = l;
+j = l1;
+}
+}
 }
 }
 }
 m = 0;
-for (k = v; k < l1; k++)
+for (y = v; y < l1; y++)
 {
-g[m] = haystack[k];
+g[m] = haystack[y];
 m++;
 }
-if (haystack != g)
-{
 haystack = g;
 return (haystack);
-}
-else
-return (NULL);
 }
