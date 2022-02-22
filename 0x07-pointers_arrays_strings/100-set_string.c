0810364ff2ea79290cb7d9766920c59458d0fe52
hichem <hichemmaatoug55@gmail.com>
2022-02-22 11:00:02 -0800

    r

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * set_string - writes the character c to stdout
+ * @s:string
+ * @to:string
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void set_string(char **s, char *to)
+{
+*s = to;
+}
