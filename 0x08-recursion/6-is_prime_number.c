22896d86f1771c36bcb44366d1f365748543ee4c
hichem <hichemmaatoug55@gmail.com>
2022-02-23 10:12:30 -0800

    f

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * is_prime_number - p
+ * @n:int
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int is_prime_number(int n)
+{
+if (n % 2 == 0 || n < 2)
+return (0);
+else
+return (1);
