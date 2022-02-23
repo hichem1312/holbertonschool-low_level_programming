7c1c533c8b9515f87d9ff5353bb8bfb7236d61ec
hichem <hichemmaatoug55@gmail.com>
2022-02-23 02:57:05 -0800

    r

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen_recursion - o
+ * @s:string
+ */
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+return (0);
+else
+return 1 + _strlen_recursion(s + 1);
+}
