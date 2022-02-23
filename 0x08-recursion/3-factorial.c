12126e0a1bfbb877e95dc6ee5356de95b4a3c9e0
hichem <hichemmaatoug55@gmail.com>
2022-02-23 04:49:20 -0800

    w

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * factorial - copy tow strings
+ * @n:string
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int factorial(int n)
+{
+if (n >= 1)
+return (n * factorial(n - 1));
+else if (n < 0)
+return -1;
+else
+return 1;
+}
