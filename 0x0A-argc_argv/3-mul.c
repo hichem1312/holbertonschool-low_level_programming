edbff03817a5aa1185814c3e3350feb42f9c7b49
hichem <hichemmaatoug55@gmail.com>
2022-02-25 11:22:08 -0800

    r

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *@argc:integer
+ *@argv:string
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int s;
+if (argc == 3)
+{
+s = atoi(argv[1]) * atoi(argv[2]);
+printf("%d\n", s);
+}
+else
+printf("Error\n");
+return (1);
+}
