66879102b5f7dcfecbdab93cf0d5981e7c9df55d
hichem <hichemmaatoug55@gmail.com>
2022-02-25 02:28:23 -0800

    d

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *@argc:integer
+ *@argv:string
+ * Return: Always 0 (Success)
+ */
+int main (int argc, char *argv[])
+{
+argc = 0;
+printf("%s\n", argv[argc]);
+return (0);
+}
