4a0010ea235d46293259de48db1c1308a7fee44f
hichem <hichemmaatoug55@gmail.com>
2022-02-25 12:00:12 -0800

    e

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *@argc:integer
+ *@argv:string
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+int s, i;
+for (i = 1; i < argc; i++)
+{
+if (atoi(argv[i]) > 0)
+{
+s += atoi(argv[i]);
+}
+else
+{
+argc = 0;
+}
+}
+if (argc > 0)
+{
+printf("%d\n", s);
+}
+if (argc == 0)
+{
+printf("Error\n");
+}
+return (0);
+}
