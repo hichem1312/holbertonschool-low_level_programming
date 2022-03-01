937dbb0d88eaa71263199a1316fa93256e6afd48
hichem <hichemmaatoug55@gmail.com>
2022-03-01 05:02:17 -0800

    d

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - Entry point
+ *@width:int
+ *@height:int
+ * Return: Always 0 (Success)
+ */
+int **alloc_grid(int width, int height)
+{
+int i, j, k, x;
+int **p;
+if (width <= 0)
+	return (NULL);
+if (height <= 0)
+	return (NULL);
+
+p = malloc(sizeof(int) * height);
+if (p == NULL)
+	return (NULL);
+for (i = 0; i < height; i++)
+{
+	p[i] = malloc(sizeof(int) * width);
+	if (p[i] == NULL)
+	{
+		for (x = 0; x < i; x++)
+			free(p[x]);
+		free(p);
+		return (NULL);
+	}
+}
+for (j = 0; j < height; j++)
+{
+	for (k = 0; k < width; k++)
+		p[j][k] = 0;
+}
+return (p);
+}
