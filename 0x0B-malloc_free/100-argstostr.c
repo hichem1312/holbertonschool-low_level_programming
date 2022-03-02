dff31ce5aa8f301c242380e9d0e58c94c7d5b456
hichem <hichemmaatoug55@gmail.com>
2022-03-02 11:23:39 -0800

    d

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -10,32 +10,31 @@
  */
 char *argstostr(int ac, char **av)
 {
-int i, a, l, k, j;
+int i, a, l, s, j;
 char *str;
 if (ac == 0 || av == NULL)
 	return (NULL);
 a = ac;
 l = 0;
+s = 0;
 for (i = 0; i < a; i++)
 {
 	l = strlen(av[i]) + l;
+	for(j = 0; j < l; j++)
+	{
+		s += 1;
+	}
 }
-str = malloc(sizeof(char *) * l);
+str = malloc(sizeof(char *) * (s + ac));
 if (str == NULL)
 {
 free(str);
 return (NULL);
 }
-k = 0;
-	for (i = 0; i < ac; i++)
-	{
-		for (j = 0; j < (int)strlen(av[i]); j++)
-		{
-			str[k] = av[i][j];
-			k++;
-		}
-		str[k++] = '\n';
-	}
-	str[k] = '\0';
+for (i = 0; i < ac; i++)
+{
+	strcat(str, av[i]);
+	strcat(str, "\n");
+}
 return (str);
 }
