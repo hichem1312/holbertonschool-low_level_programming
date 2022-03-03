583ce15240563edc2ad78ea88798c72413f4b36e
hichem <hichemmaatoug55@gmail.com>
2022-03-03 05:44:12 -0800

    z

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,8 +8,22 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s;
-	unsigned int t2 = strlen(s2), t1 = strlen(s1), i, j;
+	char *s, *ch = "";
+	unsigned int t2, t1, i, j;
+	if (s1 == NULL)
+	{
+		s1 = ch;
+		t1 = 0;
+	}
+	else
+		t1 = strlen(s1);
+	if (s2 == NULL)
+	{
+		s2 = ch;
+		t2 = 0;
+	}
+	else
+		 t2 = strlen(s2);
 	if (n >= t2)
 		s = malloc(t1 + t2);
 	else
