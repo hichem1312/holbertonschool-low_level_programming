52fee453c76ed8c5f163f2ac5d7ceb968ee8ba73
hichem <hichemmaatoug55@gmail.com>
2022-03-03 05:08:13 -0800

    a

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+void *malloc_checked(unsigned int b);
+#endif /* MAIN_H */
