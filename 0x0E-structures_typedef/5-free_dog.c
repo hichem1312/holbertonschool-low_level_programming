0f39ce7dea78710e07bf3c4b589b2b482cfb80cc
hichem <hichemmaatoug55@gmail.com>
2022-03-07 09:59:02 -0800

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - cc
+ * @d: f
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+if (d != NULL)
+{
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
+}
