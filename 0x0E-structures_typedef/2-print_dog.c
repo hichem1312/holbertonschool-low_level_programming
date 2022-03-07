6706f38d3ef294e2da2d3fe8ca6765e04893424c
hichem <hichemmaatoug55@gmail.com>
2022-03-07 08:53:46 -0800

    print

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,25 @@
+#include "dog.h"
+#include "stdio.h"
+/**
+ *print_dog - Entry point
+ *@d:char
+ * Return: Always 0 (Success)
+ */
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+			printf("Name: %s\n", d->name);
+		else
+			printf("Name: (nil)");
+		if (d->age >= 0.0)
+			printf("Age: %f\n", d->age);
+		else
+			printf("Age: (nil)");
+		if (d->owner != NULL)	
+			printf("Owner: %s\n", d->owner);
+		else
+			printf("Owner: (nil)");
+	}
+}
