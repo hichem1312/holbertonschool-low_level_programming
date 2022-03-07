7c05af5dd15584fb2f7efc9a3997129bf56b4a00
hichem <hichemmaatoug55@gmail.com>
2022-03-07 09:54:17 -0800

    d

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,32 +11,24 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-struct dog *d;
-char *n;
-char *o;
-if ((name != NULL) && (age >= 0) && (owner != NULL))
+dog_t *d;
+d = malloc(sizeof(dog_t));
+if (d == NULL)
+	return (NULL);
+else
 {
-	d = malloc(sizeof(struct dog));
-	if (name != NULL)
-		n = strdup(name);
-	else
-		return (NULL);
-	if (owner != NULL)
-		o = strdup(owner);
-	else
-		return (NULL);
-	if ((d == NULL) || (n == NULL) || (o == NULL))
-	{
-		free(o);
-		free(n);
-		free(d);
-		return (NULL);
-	}
-	d->name = n;
-	d->owner = o;
+	d->name = malloc(strlen(name) + 1);
+	d->owner = malloc(strlen(owner) + 1);
+}
+if (d->name == NULL || d->owner == NULL)
+{
+	free(d->owner);
+	free(d->name);
+	free(d);
+	return (NULL);
+}
+	d->name = strcpy(d->name, name);
+	d->owner = strcpy(d->owner, owner);
 	d->age = age;
 	return (d);
-}
-else
-return (NULL);
 }
