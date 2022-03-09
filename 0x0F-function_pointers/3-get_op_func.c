8bb61060930ebae4c018d9bb1744c9fcd30635ce
hichem <hichemmaatoug55@gmail.com>
2022-03-09 11:16:29 -0800

    d

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,28 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * get_op_func - op
+ * @s: char
+ * Return: int 
+ */
+int (*get_op_func(char *s))(int, int)
+{
+int i;
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}};
+i = 0;
+while (i < 5)
+{
+	if (strcmp(s, ops[i].op) == 0)
+		return(ops[i].f);
+i++;
+}
+return (0);
+}
