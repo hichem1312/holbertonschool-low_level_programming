dfdfd9e22c435bcf3e3d3bd090eb8d4b3673462b
hichem <hichemmaatoug55@gmail.com>
2022-03-09 09:53:30 -0800

    d

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - prints a name
+ * @array: n
+ * @size: f
+ * action:function
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+size_t i;
+for (i = 0; i < size; i++)
+	action(array[i]);
+}
