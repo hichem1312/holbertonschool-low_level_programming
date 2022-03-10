f7ab970ccaaa28f8b7ab814d4b9fb3c6cb760d99
hichem <hichemmaatoug55@gmail.com>
2022-03-09 16:26:13 -0800

    sum

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - p
+ * @n: int
+ * Return: int
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i;
+int s;
+va_list x;
+if (n == 0)
+	return (0);
+va_start(x, n);
+s = 0;
+	for (i = 0; i < n; i++)
+		s = va_arg(x, int) + s;
+va_end(x);
+return (s);
+}
