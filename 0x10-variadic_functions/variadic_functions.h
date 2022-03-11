c763851150c1e6390a36497326b8f4dc7438b618
hichem <hichemmaatoug55@gmail.com>
2022-03-11 03:19:55 -0800

    e

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,7 +9,7 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 /**
- * st - p
+ * struct st - p
  * @ch: int
  * @f: char
  * Return: void
