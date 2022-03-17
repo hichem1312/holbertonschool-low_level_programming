bc6c1a1901fca04de27e86e8e4ff167e7a4accbc
hichem <hichemmaatoug55@gmail.com>
2022-03-17 14:16:59 -0700

    s

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+* print_first - h
+**/
+void __attribute__ ((constructor)) print_first(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
