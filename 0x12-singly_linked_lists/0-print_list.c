b8a1a58bdb95631cf5026d9e2fbe8b49261bff9f
hichem <hichemmaatoug55@gmail.com>
2022-03-17 09:53:46 -0700

    x

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,14 +2,17 @@
 /**
  * print_list - print list
  * @h: str
- * */
+ * Return: number of nodes
+ */
 size_t print_list(const list_t *h)
 {
 	int i = 0;
+
 	if (h == NULL)
 		return (0);
 	while (h != NULL)
-	{	if (h->str == NULL)
+	{
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
