4eb286fe54c1081fb055b781a6841296b3d64422
hichem <hichemmaatoug55@gmail.com>
2022-03-17 09:51:14 -0700

    x

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,20 +2,20 @@
 /**
  * list_len - number of elements linked
  * @h: str
- * */
+ * Return: the number of nodes
+ */
 size_t list_len(const list_t *h)
 {
 	int i = 0;
+
 	if (h == NULL)
 		return (0);
 	while (h != NULL)
-	{	if (h->str == NULL)
+	{
+		if (h->str == NULL)
 			return (i);
-		else
-		{
-			h = h->next;
-			i++;
-		}
+		h = h->next;
+		i++;
 	}
 	return (i);
 }
