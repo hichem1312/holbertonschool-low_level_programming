1c9f30f0309fef1ad7499c80ef86de7b2d9db813
hichem <hichemmaatoug55@gmail.com>
2022-03-17 09:39:02 -0700

    s

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_list - print list
+ * @h: str
+ * */
+size_t print_list(const list_t *h)
+{
+	int i = 0;
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{	if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
