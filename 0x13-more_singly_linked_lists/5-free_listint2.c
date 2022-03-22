8aa4be10e95271b9d73e148300b7c45e54171a18
hichem <hichemmaatoug55@gmail.com>
2022-03-22 11:16:09 -0700

    x

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,14 +6,16 @@
  */
 void free_listint2(listint_t **head)
 {
+listint_t *ptr;
 if (!*head)
 return;
 else
 {
-while ((*head)->next != NULL)
+while ((*head) != NULL)
 {
+	ptr = (*head)->next;
 	free(*head);
-	*head = (*head)->next;
+	*head = ptr;
 }
 *head = NULL;
 }
