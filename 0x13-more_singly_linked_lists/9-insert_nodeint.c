3356585b6f04485e289a74f1f48898556092bea0
hichem <hichemmaatoug55@gmail.com>
2022-03-23 05:03:57 -0700

    s

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,41 +8,40 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *ptr = NULL;
-	listint_t *new;
-	unsigned int i = 0;
-
-	if (head)
+listint_t *ptr;
+listint_t *new_idx;
+unsigned int i = 0;
+ptr = NULL;
+if (head)
+{
+	new_idx = malloc(sizeof(listint_t));
+	if (new_idx == NULL)
+		return (NULL);
+	new_idx->n = n;
+	new_idx->next = NULL;
+	if (idx != 0)
 	{
-		new = malloc(sizeof(listint_t));
-		if (new == NULL)
-			return (NULL);
-		new->n = n;
-		new->next = NULL;
-
-		if (idx != 0)
+		ptr = *head;
+		while (ptr)
 		{
-			ptr = *head;
-			while (ptr)
+			if (i == idx - 1)
 			{
-				if (i == idx - 1)
-				{
-					new->next = ptr->next;
-					ptr->next = new;
-					return (new);
-				}
-				ptr = ptr->next;
-				i++;
+				new_idx->next = ptr->next;
+				ptr->next = new_idx;
+				return (new_idx);
 			}
-			if (idx > i)
-				return (NULL);
+			ptr = ptr->next;
+			i++;
 		}
-		else
-		{
-			new->next = *head;
-			*head = new;
-		}
-		return (new);
+		if (idx > i)
+			return (NULL);
 	}
-	return (NULL);
+	else
+	{
+		new_idx->next = *head;
+		*head = new_idx;
+	}
+	return (new_idx);
+}
+return (NULL);
 }
