4e33e43d059b6a7c2894a4a2e97c0d3104778978
hichem <hichemmaatoug55@gmail.com>
2022-03-25 04:47:13 -0700

    d

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/*
+ * binary_to_uint- converts a binary number to an int
+ * @b: is pointing to a string of 0 and 1
+ * Return: the converted number
+ **/
+unsigned int binary_to_uint(const char *b)
+{
+unsigned int i, sum = 0, s = 1, length;
+if (!b)
+	return (0);
+length = strlen(b);
+for (i = length; i > 0; i--)
+{
+	if (b[i - 1] == 49)
+		sum = sum + s;
+	else if (b[i - 1] != 48)
+		return (0);
+	s = s * 2;
+}
+return (sum);
+}
