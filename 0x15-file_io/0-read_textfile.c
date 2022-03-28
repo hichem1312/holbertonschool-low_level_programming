98bc3e2a7a0aa57ec89c02cd8326c133b02399bd
hichem <hichemmaatoug55@gmail.com>
2022-03-28 13:54:21 -0700

    s

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * read_textfile - function that reads a text file and prints it
+ * @filename: name of the file
+ * @letters: size of letters
+ * Return: the actual number of letters it could read and print
+ * */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+int a, b;
+char *buf = malloc(letters);
+if (!filename)
+	return (0);
+a = open(filename, O_RDONLY);
+if (a == -1)
+	return (0);
+b = read(a, buf, letters);
+write(a, buf, letters);
+close(a);
+return (b);
+}
