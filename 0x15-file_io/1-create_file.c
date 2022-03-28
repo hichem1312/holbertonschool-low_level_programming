222009212cc7b9bb1d2203187480d3a13b47c023
hichem <hichemmaatoug55@gmail.com>
2022-03-28 14:52:05 -0700

    s

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * create_file- function that creates a file
+ * @filename: the name of the file to create
+ * @text_content: is a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure 
+ */
+int create_file(const char *filename, char *text_content)
+{
+int n;
+if (!filename)
+	return (-1);
+n = open(filename, 0600);
+if (n == -1)
+	return (-1);
+if (text_content)
+	write(n, text_content, strlen(text_content));
+else
+	return (-1);
+close(n);
+return (1);
+}
