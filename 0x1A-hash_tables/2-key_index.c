99d4693da263991f8b935a4fa361bb5f632923b0
hichem <hichemmaatoug55@gmail.com>
2022-05-29 16:59:51 -0700

    return a index of a key

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,6 +3,7 @@
  * key_index - gives the index of a key
  * @key: is the key
  * @size: is the size of the array of the hash table
+ * Return: index
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
